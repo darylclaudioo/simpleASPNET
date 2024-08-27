681ce9ab3582797d0b8e4eedfa8888096716253b
darylclaudioo <darylclaudio2805@gmail.com>
2024-08-28 01:20:32 +0700

    init

---
+++ ASPNETRestaurantWebsite/ASPNETRestaurantWebsite/app.ts
@@ -0,0 +1,18 @@
+﻿function TSButton() {
+    var name = "Muhammad";
+    document.getElementById("ts-example").innerHTML = greeter(user);
+}
+var Student = /** @class */ (function () {
+    function Student(firstName, middleName, lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleName + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.middleName + " " + person.lastName;
+}
+var user = new Student("Muhammad", "Ichsan", "Ridho");
+//# sourceMappingURL=app.js.map
